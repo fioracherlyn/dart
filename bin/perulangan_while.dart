c3b02b349b3474bdf392303b17c86d2373ddd814
Fiora Cherlyn <fiochrlyn@gmail.com>
2023-08-24 08:38:25 +0700

    membuat perulangan_while.dart

--- bin/perulangan_while.dart
+++ bin/perulangan_while.dart
@@ -2,4 +2,14 @@ import 'dart:io';
 
 void main(List<String> args) {
   int i = 0;
+  bool ulang = true;
+
+  while(ulang) {
+    stdout.write("Apakah anda mau keluar (y/t): ");
+    String jawaban = stdin.readLineSync()!;
+
+    i++;
+    if(jawaban.toUpperCase() == "Y") ulang = false;
+  }
+  print("total perulangan: $i");
 }
