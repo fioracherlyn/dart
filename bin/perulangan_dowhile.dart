95425a2b5748d231fb042b550170f828d6f92558
Fiora Cherlyn <fiochrlyn@gmail.com>
2023-08-24 08:52:04 +0700

    initial commit

--- bin/perulangan_dowhile.dart
+++ bin/perulangan_dowhile.dart
@@ -1,1 +1,16 @@
 import 'dart:io';
+
+void main(List<String> args) {
+  int  i = 0;
+  bool ulang = true;
+
+  do {
+    stdout.write("Apakah anda mau keluar (y/t)");
+    String jawaban = stdin.readLineSync()!;
+
+    i++;
+    if(jawaban.toUpperCase() == "Y") ulang = false;
+  } while (ulang);
+
+  print("Total perulangan $i");
+}
