32db15956ae6af79c16d8afa8d170765b0deb471
Fiora Cherlyn <fiochrlyn@gmail.com>
2023-08-24 08:35:33 +0700

    add file

---
+++ bin/control_flow.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  stdout.write("jumlah perulangan: ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= n; i++) {
+    print("perulangan ke: ");
+  }
+}
